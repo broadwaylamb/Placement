9585111fa24e6ca0a782f682236a9406f5383cca
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2025-09-14 15:14:55 +0200

    Add a demo with PlacementThatFits being used inside an HStack
    Currently it's displayed incorrectly. The next commit
    will fix it.

--- Sources/DemoApp/DemoViews/PlacementThatFitsDemo.swift
+++ Sources/DemoApp/DemoViews/PlacementThatFitsDemo.swift
@@ -15,25 +15,52 @@ struct PlacementThatFitsDemo: View {
     var body: some View {
         ScrollView {
             VStack(spacing: 20) {
-                PlacementThatFits(prefersViewThatFits: false) {
-                    Button("Some longer text that wont fit initially") {
-                      print("tap index 0")
-                    }.onAppear(perform: {
-                      print("did appear index 0")
-                    })
-                    Button("A button") {
-                      print("tap index 1")
-                    }.onAppear(perform: {
-                      print("did appear index 1")
-                    })
-                }
-                .border(.red)
-                .frame(maxWidth: changeContainerWidth ? .infinity : 150)
-                
+                Text("Layout below uses SwiftUI.ViewThatFits (on iOS 16)")
+                    .fontWeight(.semibold)
+                Demo(changeContainerWidth: changeContainerWidth, prefersViewThatFits: true)
+
+                Text("Layout below uses Placement")
+                    .fontWeight(.semibold)
+                Demo(changeContainerWidth: changeContainerWidth, prefersViewThatFits: false)
+
                 Button("Change container width") {
                     changeContainerWidth.toggle()
                 }
             }.frame(maxWidth: .infinity)
         }
+    }
+}
+
+private struct Demo: View {
+    var changeContainerWidth: Bool
+    var prefersViewThatFits: Bool
+
+    var body: some View {
+        PlacementThatFits(prefersViewThatFits: prefersViewThatFits) {
+            Button("Some longer text that wont fit initially") {
+              print("tap index 0")
+            }.onAppear(perform: {
+              print("did appear index 0")
+            })
+            Button("A button") {
+              print("tap index 1")
+            }.onAppear(perform: {
+              print("did appear index 1")
+            })
+        }
+        .border(.red)
+        .frame(maxWidth: changeContainerWidth ? .infinity : 150)
+
+        HStack {
+            Text("Leading view")
+            Color.red
+                .frame(width: 1)
+            PlacementThatFits(prefersViewThatFits: prefersViewThatFits) {
+                Text("Trailing view with long text")
+                Text("Trailing view")
+            }
+        }
+        .border(.red)
+        .frame(maxWidth: changeContainerWidth ? .infinity : 150)
     }
 }
